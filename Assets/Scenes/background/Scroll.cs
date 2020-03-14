0e8c280257ae31c8f79da2135b398d8252c8271c
bruno-reload <bruno-reload@live.com>
2020-03-14 16:52:47 -0300

    start

---
+++ Assets/Scenes/background/Scroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Scroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Renderer back;
+    private float vel;
+    private string nomeBack;
+
+    private void Start() {
+        back = GetComponent<Renderer>();
+        vel = 0.0f;
+        nomeBack = tag;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        switch (nomeBack)
+        {
+            case "background":
+                vel = 0.04f;
+                break;
+            case "foreground":
+                vel = 0.02f;
+                break;
+            case "street":
+                vel = 0.05f;
+                break;
+        }   
+        Vector2 offset = new Vector2(vel * Time.deltaTime, 0);
+        back.material.mainTextureOffset += offset;
+    }
+}
---
+++ Assets/Scenes/player/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] character;
+    Animator animator;
+    private float time;
+    private Transform[] characterInstances;
+    void Start()
+    {
+        time = 0.0f;
+        characterInstances = new Transform[character.Length];
+
+        for(int i = 0; i < character.Length; i++){
+            characterInstances[i] = Instantiate(character[i]);
+            characterInstances[i].parent = transform;
+            characterInstances[i].position += transform.position;
+        }
+        animator = characterInstances[0].GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.DownArrow)){
+            changeCharacter();
+        }
+    }
+    void changeCharacter(){
+        Transform last = character[0];
+        time += Time.deltaTime;
+
+        character[0] = character[1];
+        character[1] = character[2];
+        character[2] = last;
+
+
+        for (int i = 0; i < character.Length; i++){
+            characterInstances[i].position = character[i].position + transform.position; 
+        }
+        animator.SetTrigger("go_to_change");
+    }
+}
