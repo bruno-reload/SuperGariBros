7aca2a9d1588484c642c75d4dccdfe6abfe7b667
bruno-reload <bruno-reload@live.com>
2020-03-27 17:11:41 -0300

    teste tween

---
+++ Assets/Scenes/ui/PainelSlide.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PainelSlide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Init();
+        transform.DOMove(new Vector3(0.0f,0.0f,0.0f),5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
