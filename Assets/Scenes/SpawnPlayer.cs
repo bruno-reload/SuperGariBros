0ca9a954b4a11f6f583e0a0274d4c5b4ab53fcee
bruno-reload <bruno-reload@live.com>
2020-04-08 17:32:19 -0300

    alterações nas animações primeira tentativa

--- Assets/Scenes/SpawnPlayer.cs
+++ Assets/Scenes/SpawnPlayer.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class SpawnPlayer : MonoBehaviour {
-    private Vector3 highlightPosition;
+public class SpawnPlayer : MonoBehaviour
+{
     public float foward;
-    private Animator animator;
+    private Animator[] animator;
     private bool inAnimation = false;
     private float speed = SpeedControl.speed;
     public float spawnRot;
@@ -14,78 +14,102 @@ public class SpawnPlayer : MonoBehaviour {
     public float distanceAvatars;
     private int bottom = 0;
     private int size = 0;
+    private IEnumerator[] moviment;
     private Vector3[] buffPosition;
-    void Start () {
+    void Start()
+    {
 
-        size = PoolControll.poolPlayer.allItems ().Length;
+        size = PoolControll.poolPlayer.allItems().Length;
+        animator = new Animator[size];
         buffPosition = new Vector3[size];
+        moviment = new IEnumerator[size];
 
-        setStartPos ();
+        setStartPos();
 
-        animator = PoolControll.poolPlayer.getItem (0).GetComponent<Animator> ();
-        transform.localRotation = Quaternion.Euler (0.0f, 0.0f, spawnRot);
+        transform.localRotation = Quaternion.Euler(0.0f, 0.0f, spawnRot);
     }
-    private void setStartPos () {
+    private void setStartPos()
+    {
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
+        {
             float r = distanceAvatars * i;
             float d = rotationAvatars * Mathf.Deg2Rad;
 
-            PoolControll.poolPlayer.getItem (i).transform.position = transform.position + new Vector3 (
-                r * Mathf.Cos (d) - r * Mathf.Sin (d),
-                r * Mathf.Sin (d) + r * Mathf.Cos (d), 0.0f);
-            buffPosition[i] = PoolControll.poolPlayer.getItem (i).transform.position;
+            PoolControll.poolPlayer.getItem(i).transform.position = transform.position + new Vector3(
+                r * Mathf.Cos(d) - r * Mathf.Sin(d),
+                r * Mathf.Sin(d) + r * Mathf.Cos(d), 0.0f);
+            buffPosition[i] = PoolControll.poolPlayer.getItem(i).transform.position;
+            animator[i] = PoolControll.poolPlayer.getItem(i).GetComponentInChildren<Animator>();
         }
     }
-    void Update () {
-        if (Input.GetKeyDown (KeyCode.DownArrow) && !inAnimation) {
-            swapPosition ();
-            for (int i = 0; i < size; i++) {
-                StartCoroutine (moveToTarget (i));
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !inAnimation)
+        {
+            foreach (Animator item in animator)
+            {
+                item.SetInteger("loopState",3);
+                Debug.Log("loop");
             }
-            // animator.SetTrigger("go_to_change");
+            swapPosition();
+            for (int i = 0; i < size; i++)
+            {
+                moviment[i] = moveToTarget(i);
+                StartCoroutine(moviment[i]);
+            }
 
         }
     }
-    private void swapPosition () {
+    private void swapPosition()
+    {
         Vector3 last = buffPosition[0];
         bottom = (bottom + 2) % size;
-        
-        for (int i = 0; i < size - 1; i++) {
+
+        for (int i = 0; i < size - 1; i++)
+        {
             buffPosition[i] = buffPosition[i + 1];
-            PoolControll.poolPlayer.getItem (i).GetComponent<Player> ().bottomLane = false;
+            PoolControll.poolPlayer.getItem(i).GetComponent<Player>().bottomLane = false;
         }
-        PoolControll.poolPlayer.getItem (size - 1).GetComponent<Player> ().bottomLane = false;
-        PoolControll.poolPlayer.getItem (bottom).GetComponent<Player> ().bottomLane = true;
+        PoolControll.poolPlayer.getItem(size - 1).GetComponent<Player>().bottomLane = false;
+        PoolControll.poolPlayer.getItem(bottom).GetComponent<Player>().bottomLane = true;
         buffPosition[size - 1] = last;
     }
-    private IEnumerator moveToTarget (int i) {
+    private IEnumerator moveToTarget(int i)
+    {
+        Debug.Log("lerpt");
         inAnimation = true;
-        if (PoolControll.poolPlayer.getItem (i).GetComponent<Player> ().bottomLane) {
+        if (PoolControll.poolPlayer.getItem(i).GetComponent<Player>().bottomLane)
+        {
             float x = buffPosition[i].x;
             float y = buffPosition[i].y;
 
-            Vector3 buff = new Vector3 (x * foward, y * foward, 0.0f);
+            Vector3 buff = new Vector3(x * foward, y * foward, 0.0f);
 
-            while (Vector3.Distance (PoolControll.poolPlayer.getItem (i).transform.localPosition, buff) > 0.05f) {
-                PoolControll.poolPlayer.getItem (i).transform.localPosition = Vector3.Lerp (
-                    PoolControll.poolPlayer.getItem (i).transform.localPosition,
+
+            while (Vector3.Distance(PoolControll.poolPlayer.getItem(i).transform.localPosition, buff) > 0.05f)
+            {
+                PoolControll.poolPlayer.getItem(i).transform.localPosition = Vector3.Lerp(
+                    PoolControll.poolPlayer.getItem(i).transform.localPosition,
                     buff,
                     speed * Time.deltaTime * 2);
                 yield return null;
             }
         }
-        while (Vector3.Distance (PoolControll.poolPlayer.getItem (i).transform.localPosition, buffPosition[i]) > 0.05f) {
-            PoolControll.poolPlayer.getItem (i).transform.localPosition = Vector3.Lerp (
-                PoolControll.poolPlayer.getItem (i).transform.localPosition,
+        while (Vector3.Distance(PoolControll.poolPlayer.getItem(i).transform.localPosition, buffPosition[i]) > 0.05f)
+        {
+            PoolControll.poolPlayer.getItem(i).transform.localPosition = Vector3.Lerp(
+                PoolControll.poolPlayer.getItem(i).transform.localPosition,
                 buffPosition[i],
                 speed * Time.deltaTime);
             yield return null;
         }
-        if (i == size - 1) {
+        if (i == size - 1)
+        {
 
             inAnimation = false;
         }
-        StopCoroutine (moveToTarget (i));
+        animator[i].SetInteger("loopState",0);
+        StopCoroutine(moviment[i]);
     }
 }
--- Assets/Scenes/player/Player.cs
+++ Assets/Scenes/player/Player.cs
@@ -8,6 +8,7 @@ public class Player : ItemPool
     private GameObject lifeBar;
     private GameObject pointsValue;
     private float lerpSpeed;
+    private Animator animator;
     public bool bottomLane = false;
 
     private IEnumerator p;
@@ -18,27 +19,32 @@ public class Player : ItemPool
         lerpSpeed = SpeedControl.speed;
         lifeBar = GameObject.Find("Canvas/lifeBar/life");
         pointsValue = GameObject.Find("Canvas/pointBar/value");
+        animator = GetComponentInChildren<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (combine == other.GetComponent<Trash>().type)
         {
+            animator.SetInteger("loopState", 2);
             pointsValue.GetComponent<Points>().addPoints();
         }
         else
         {
-            p = PlayerNotificationCollider();
+            //p = PlayerNotificationCollider();
             l = lifeBar.GetComponent<LifeControll>().lifeBarNotificationCollider();
             c = lifeBar.GetComponent<LifeControll>().changeSizeBar();
 
-            StartCoroutine(p);
+            animator.SetInteger("loopState", 1);
+
+            //StartCoroutine(p);
             StartCoroutine(l);
             StartCoroutine(c);
         }
+        animator.SetInteger("loopState", 0);
     }
     private IEnumerator PlayerNotificationCollider()
     {
-        Color col = GetComponent<Renderer>().material.color;
+        Color col = GetComponentInChildren<Renderer>().material.color;
 
         GetComponent<Renderer>().material.color = Color.red;
         yield return new WaitForSeconds(0.05f);
@@ -47,9 +53,11 @@ public class Player : ItemPool
         yield return new WaitForSeconds(0.1f);
 
         GetComponent<Renderer>().material.color = col;
-        
+
         StopCoroutine(p);
         StopCoroutine(l);
         StopCoroutine(c);
+
+        animator.SetInteger("loopState", 0);
     }
 }
