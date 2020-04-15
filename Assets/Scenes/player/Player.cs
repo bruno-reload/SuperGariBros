65f13c609c3ea100e8e8bf362fd80dc482d3976c
bruno correia <correia.norte@gmail.com>
2020-04-15 15:15:45 -0300

    sons e movimentação estão funcionando razoavelmente bem vou dar uma melhorada na maquina de estados das animações agora

--- Assets/Scenes/player/Player.cs
+++ Assets/Scenes/player/Player.cs
@@ -2,48 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class Player : ItemPool
 {
+
+    [SerializeField]
+    public Sound[] sounds;
     public TrashType combine;
     private GameObject lifeBar;
     private GameObject pointsValue;
     private float lerpSpeed;
     private Animator animator;
     public bool bottomLane = false;
-    private IEnumerator p;
+    private AudioSource audioSource;
     private IEnumerator l;
     private IEnumerator c;
-    private AudioSource audioSource;
-    public AudioClip audioCollider;
-    public AudioClip audioCollect;
+    private IEnumerator w;
+
+    private float startTime = 0;
+    private float clipTime = 0;
     private void Start()
     {
+
         lerpSpeed = SpeedControl.speed;
         lifeBar = GameObject.Find("Canvas/lifeBar/life");
         pointsValue = GameObject.Find("Canvas/pointBar/value");
         animator = GetComponentInChildren<Animator>();
+
+        gameObject.AddComponent<AudioSource>();
         audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            sounds[i].setSource(audioSource);
+            if (sounds[i].name == "walk") sounds[i].play();
+        }
     }
+    private void Update()
+    {
+        if ((Time.time - startTime) >= clipTime && !audioSource.isPlaying)
+        {
+            foreach (Sound item in sounds)
+            {
+                if (item.name == "walk") item.play();
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        startTime = Time.time;
         if (combine == other.GetComponent<Trash>().type)
         {
-            animator.ResetTrigger("run");
-            animator.ResetTrigger("change");
-            animator.ResetTrigger("idle");
-            animator.ResetTrigger("collision");
+            foreach (Sound item in sounds)
+            {
+                if (item.name == "collect") clipTime = item.oneShotPlay();
+            }
             animator.SetTrigger("collect");
 
             pointsValue.GetComponent<Points>().addPoints();
-            audioSource.PlayOneShot(audioCollect,5);
+
         }
         else
         {
-            animator.ResetTrigger("collect");
-            animator.ResetTrigger("run");
-            animator.ResetTrigger("change");
-            animator.ResetTrigger("idle");
+            foreach (Sound item in sounds)
+            {
+                if (item.name == "collision") clipTime = item.oneShotPlay();
+            }
             animator.SetTrigger("collision");
 
 
@@ -52,8 +77,8 @@ public class Player : ItemPool
 
             StartCoroutine(l);
             StartCoroutine(c);
-            
-            audioSource.PlayOneShot(audioCollider,5);
         }
+
     }
 }
+
--- Assets/Scenes/sound/SoundController.cs
+++ Assets/Scenes/sound/SoundController.cs
@@ -7,6 +7,8 @@ public class SoundController : MonoBehaviour
     // Start is called before the first frame update
     private float count;
     private AudioSource audioSource;
+    [Range(0f, 1f)]
+    public float volume = 1f;
 
     private void Start()
     {
@@ -18,7 +20,40 @@ public class SoundController : MonoBehaviour
     {
         count += Time.deltaTime * 0.1f;
 
-        audioSource.volume = Mathf.Lerp(0.0f, 1.0f,count);
+        audioSource.volume = Mathf.Lerp(0.0f, volume, count);
 
     }
+}
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Range(0.5f, 3f)]
+    public float pitch = 1f;
+    private AudioSource source;
+
+    public void setSource(AudioSource source)
+    {
+        this.source = source;
+        source.clip = clip;
+    }
+    public float play()
+    {
+        this.source.pitch = pitch;
+        this.source.volume = volume;
+        this.source.Play();
+        return clip.length;
+    }
+    public float oneShotPlay()
+    {
+        this.source.pitch = pitch;
+        this.source.volume = volume;
+        this.source.Stop();
+        this.source.PlayOneShot(clip);
+        return clip.length;
+    }
+
 }
